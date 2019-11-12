36a3eb1d3fe0fa253909945520d9748504d79d13
H-koubou <kyouten0110@gmail.com>
2019-11-12 13:44:59 +0900

    掲示板データを保存できるように処理

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,6 +8,13 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
-  def create 
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+
+  private 
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end

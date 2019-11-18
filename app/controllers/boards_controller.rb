386af31119f9b065765156a0d29f927d59bdc199
H-koubou <kyouten0110@gmail.com>
2019-11-18 18:07:56 +0900

    BoardControllerにBeforeActionを定義

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
   def index
     @title = "Hello World"
     @description = "これはトップページになります。"
@@ -10,15 +11,33 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    @board = Board.create(board_params)
+    redirect_to @board
   end
 
   def show
-    @board = Board.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+    redirect_to board
+  end
+
+  def destroy
+    @board.delete
+    redirect_to boards_path
   end
 
   private 
   def board_params
     params.require(:board).permit(:name, :title, :body)
   end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
 end
